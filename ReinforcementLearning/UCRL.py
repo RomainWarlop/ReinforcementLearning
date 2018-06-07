a71d62329efc8573b744575fb795bd325c791d26
Romain <romain@fifty-five.com>
2018-06-07 15:21:01 +0200

    cleaning and comments

--- ReinforcementLearning/UCRL.py
+++ ReinforcementLearning/UCRL.py
@@ -41,6 +41,23 @@ def ucrl_conf_weight_r(x,conf):
 class UCRL(object):
     
     def __init__(self,alpha,states,s0,actions,reward,transition):
+        """
+        Parameters
+        =========
+        alpha: float
+            exploration parameter
+        states: list of list
+            list of all states. Each state is a list in str form eg: '[1, 1, 0]'
+        s0: list in str form
+            initial state
+        actions: list
+            list of possible actions
+        reward: function
+            function of state and action that return the reward
+        transition: function
+            function of state and action that return the next state
+        """
+        
         self.alpha = alpha
         self.states = states
         self.actions = actions
@@ -54,44 +71,45 @@ class UCRL(object):
         self.t = 0
         self.reward_sequence = []
         self.arm_sequence = []
-
+        
+        # count (action,state) occurences
         self.N = pd.DataFrame(columns=actions,index=states).fillna(0)
+        # sum (action,state) rewards
         self.R = pd.DataFrame(columns=actions,index=states).fillna(0)
+        # rewards upper bound
         self.r_hat = pd.DataFrame(columns=actions,index=states).fillna(0)
         for s in states:
             self.policy[s] = np.random.randint(0,self.cardA)
 
         # choose first state at random
         self.state = s0
-
-#    def conf_r(self,t,s,a,conf):
-#        if self.N.loc[s,a]>0:
-#           conf /= 2*self.N.loc[s,a]
-#        else:
-#            conf = 10
-#        conf = min(1,np.sqrt(conf))
-#        return conf
     
     def Bellman_a(self,a,V,gamma,str_state,rewards):
+        """
+        Bellman update for a particular action,state
+        """
         # update for one state
-        r = rewards[str_state][a]+self.conf.loc[str_state,a] # UCB
-        #next_state = self.transition(ast.literal_eval(state),a)
-        #r+=gamma*V[str(next_state)]
+        r = rewards[str_state][a]+self.conf.loc[str_state,a] # UCB part
         next_state = self.transition(str_state,a)
         r+=gamma*V[next_state]
         return r
         
     def policyUpdate(self,rewards):
-        t0 = time.time()
+        """
+        Compute policy based on current rewards estimates using Value Iteration
+        """
+        #t0 = time.time()
         V = dict.fromkeys(self.states)
         for str_state in self.states:
             V[str_state] = 0
         
+        # Value Iteration
         for it in range(self.n_iter):
             shuffle(self.states)
             for str_state in self.states:
                 V[str_state] = max(list(map(lambda a: self.Bellman_a(a,V,self.gamma,str_state,rewards),range(self.cardA))))
         
+        # Compute policy using value function
         for str_state in self.states:
             tmp = np.zeros(self.cardA)
             for a in range(self.cardA):
@@ -100,16 +118,26 @@ class UCRL(object):
                 r+=self.gamma*V[next_state]
                 tmp[a] = r
             self.policy[str_state] = np.argmax(tmp)
-        t1 = time.time()
-        tot = t1-t0
-        print("Policy update time %f seconds" % tot)
+#        t1 = time.time()
+#        tot = t1-t0
+#        print("Policy update time %f seconds" % tot)
 
-    def run(self,T=1000,gamma=1,n_iter=100):
+    def run(self,T=1000,gamma=1,n_iter=100,verbose=True):
+        """
+        Parameters
+        ========
+        T: int
+            number of iterations of UCRL
+        gamma: float
+            discount parameter of Bellman operator
+        n_iter:
+            number of iterations of Bellman operator
+        """
         self.gamma = gamma
         self.update_time = []
         self.n_iter = n_iter
         while self.t<T:
-            if self.t%10==0:
+            if (verbose) & (self.t%10==0):
                 print("iteration",self.t)
             # choose next action
             a = self.policy[self.state]
@@ -127,21 +155,42 @@ class UCRL(object):
             if (new_conf<self.conf/2).max().max():
                 update_policy = True
             self.r_hat = self.R/self.N
-            self.r_hat.fillna(0)
+            self.r_hat = self.r_hat.fillna(0)
             
             # update policy ?
             if update_policy:
                 self.round+=1
                 self.update_time.append(self.t+1)
-                print('update policy, round=',self.round)
+                if verbose:
+                    print('update policy, round=',self.round)
                 self.conf = deepcopy(new_conf)
                 self.policyUpdate(self.r_hat)
             
             self.t+=1
 
+#==============================================================================
 class linUCRL(object):
     
     def __init__(self,alpha,d,states,s0,actions,reward,transition):
+        """
+        Parameters
+        =========
+        alpha: float
+            exploration parameter
+        d: int
+            degree of the linear part
+        states: list of list
+            list of all states. Each state is a list in str form eg: '[1, 1, 0]'
+        s0: list in str form
+            initial state
+        actions: list
+            list of possible actions
+        reward: function
+            function of state and action that return the reward
+        transition: function
+            function of state and action that return the next state
+        """
+        
         self.alpha = alpha
         self.d = d # dimension -1
         l_w = 1+np.log(len(states[0]))
@@ -190,6 +239,9 @@ class linUCRL(object):
         return conf
     
     def Bellman_a(self,a,V,gamma,str_state,rewards):
+        """
+        Bellman update for a particular action,state
+        """
         # update for one state
         r = rewards[str_state][a]
         next_str_state = self.transition(str_state,a)
@@ -197,13 +249,16 @@ class linUCRL(object):
         return r
         
     def policyUpdate(self):
-        t0 = time.time()
+        """
+        Compute policy based on current rewards estimates using Value Iteration
+        """
+#        t0 = time.time()
         
         V = dict.fromkeys(self.states)
         for state in self.states:
             V[state] = 0
         
-        # compute rewards once
+        # compute rewards upper bound
         _rewards = dict.fromkeys(self.states)
         for str_state in self.states:
             _rewards[str_state] = dict.fromkeys(list(range(self.cardA)))
@@ -215,11 +270,13 @@ class linUCRL(object):
                 r = r[0][0]
                 _rewards[str_state][a] = r
         
+        # apply Value Iteration
         for it in range(self.n_iter):
             shuffle(self.states)
             for str_state in self.states:
                 V[str_state] = max(list(map(lambda a: self.Bellman_a(a,V,self.gamma,str_state,_rewards),range(self.cardA))))
         
+        # find policy
         for str_state in self.states:
             state = state_unstr(str_state)
             r_arms = np.zeros(self.cardA)
@@ -232,11 +289,24 @@ class linUCRL(object):
                 r+=self.gamma*V[next_state]
                 r_arms[a] = r
             self.policy[str_state] = np.argmax(r_arms)
-        t1 = time.time()
-        tot = t1-t0
-        print("Policy update time %f seconds" % tot)
+#        t1 = time.time()
+#        tot = t1-t0
+#        print("Policy update time %f seconds" % tot)
     
     def run(self,T=1000,lbda=0.5,gamma=1,n_iter=100):
+        """
+        Parameters
+        ========
+        T: int
+            number of iterations of UCRL
+        lbda: float
+            regularization for linear model learning
+        gamma: float
+            discount parameter of Bellman operator
+        n_iter:
+            number of iterations of Bellman operator
+        """
+        
         self.V = dict.fromkeys(self.actions)
         for a in self.actions:
             self.V[a] = lbda*np.eye(self.d+1)
